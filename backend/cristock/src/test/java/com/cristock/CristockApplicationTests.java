38782ff778dca6f2faf04a065fa10a7574d84e05
omd-believe <omdeshmukh240@gmail.com>
2026-06-27 15:49:01 +0530

    Intial set up

---
+++ backend/cristock/src/test/java/com/cristock/CristockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CristockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
